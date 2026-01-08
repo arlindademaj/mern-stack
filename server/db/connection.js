616c4f8f228cc120b6b685be8d320d09dd7183fc
arlindademaj <aarlindademaj@gmail.com>
2026-01-08 03:01:33 +0100

    feat: setup MongoDB connection

---
+++ server/db/connection.js
@@ -0,0 +1,30 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a pinf to cofirm a successful connection
+  await client.db("admin").command({
+    ping: 1,
+  });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
